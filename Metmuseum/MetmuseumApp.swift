d29672da9aae062004220669e29cf6841d4f7428
tom <tommy.yohanes@icloud.com>
2024-08-11 22:29:51 +0700

    Initial Commit

---
+++ Metmuseum/MetmuseumApp.swift
@@ -0,0 +1,17 @@
+//
+//  MetmuseumApp.swift
+//  Metmuseum
+//
+//  Created by tomtomtom on 8/11/24.
+//
+
+import SwiftUI
+
+@main
+struct MetmuseumApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
